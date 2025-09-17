02a9f9aff589d1dd4d13fd26d1c974c284fe6b8a
Tolga Taner <dev.tolgataner@icloud.com>
2025-09-17 03:00:18 +0300

    change naming

--- alfred.rb
+++ alfred.rb
@@ -1,11 +1,15 @@
-class Alfred < Formula
+class Alfredtt < Formula
   desc "A Swift-based CLI tool"
   homepage "https://github.com/TolgaTaner/Alfred-main"
-  url "https://github.com/TolgaTaner/Alfred-main/releases/download/v1.0.0/alfred"
+  url "https://github.com/TolgaTaner/Alfred-main/releases/download/v1.0.0/Alfred"
   sha256 "f7314f7ef78bd6dd418e3eed62c029fe2fb85748"
   version "1.0.0"
 
   def install
-    bin.install "alfred"
-  end
+      bin.install "Alfredtt"
+    end
+
+    test do
+      system "#{bin}/Alfredtt", "--version"
+    end
 end
