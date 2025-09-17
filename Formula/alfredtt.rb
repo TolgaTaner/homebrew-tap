b1ebccecd6d3e7846707d0b0094cbbcf2c50aa62
Tolga Taner <dev.tolgataner@icloud.com>
2025-09-17 03:21:24 +0300

    change formula

--- Formula/alfredtt.rb
+++ Formula/alfredtt.rb
@@ -1,7 +1,7 @@
 class Alfredtt < Formula
   desc "A Swift-based CLI tool"
   homepage "https://github.com/TolgaTaner/Alfred-main"
-  url "https://github.com/TolgaTaner/Alfred-main/releases/download/v1.0.0/Alfred"
+  url "https://github.com/TolgaTaner/Alfred-main/releases/download/1.0.0/Alfred"
   sha256 "9b4aca25b1fe4798fc26645002618e4ca374199e0f933bb9e0ad49d01429033b"
   version "1.0.0"
 
